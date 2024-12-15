9bcbbcfe1f41a1fb4b01aeff7e3c1f2963eab186
WholeCream <38162078+eprosync@users.noreply.github.com>
2024-12-15 18:10:21 -0500

    Runtimes can now return false on load to prevent loading

--- src/reflection.lua
+++ src/reflection.lua
@@ -249,12 +249,17 @@ function reflection.execute(source, name)
 
         local callback = err["on_loaded"]
         if type(callback) == "function" then
+            local nameid = self.nameid(tracker)
             local ran, err = xpcall(callback, debug.traceback, tracker, self.session)
             if not ran then
-                local nameid = self.nameid(tracker)
                 print("[Reflection] ERROR: From " .. nameid .. " -> " .. err)
                 print("[Reflection] Not adding to runtime due to error")
                 return false, "on_loaded", err
+            end
+
+            if err == false then
+                print("[Reflection] " .. nameid .. " returned false on load, so it won't be ran")
+                return true, err
             end
         end
         
